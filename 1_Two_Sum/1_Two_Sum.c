e185e9471f1059185e69ac663a27b14cfd3e0837
talengu <talenapp@gmail.com>
2017-03-19 09:58:39 +0800

    1_two_sum已经解决，添加2_add_two_numbers
    1_two_sum不是最优解

--- 1_Two_Sum/1_Two_Sum.c
+++ 1_Two_Sum/1_Two_Sum.c
@@ -1,12 +1,13 @@
 // question: https://leetcode.com/problems/two-sum/#/description
-/* 
+/*
    Given an array of integers, return indices of the two numbers such that
-   they add up to a specific target. You may assume that each input would have 
+   they add up to a specific target. You may assume that each input would have
    exactly one solution, and you may not use the same element twice.
 
-   Example: Given nums = [2, 7, 11, 15], target = 9, Because nums[0] + nums[1] 
+   Example: Given nums = [2, 7, 11, 15], target = 9, Because nums[0] + nums[1]
    = 2 + 7 = 9, return [0, 1]. */
 // talen
+//Test_OK 2017/3/18
 
 #include <stdio.h>
 
@@ -15,42 +16,31 @@
  */
 int *twoSum(int *nums, int numsSize, int target)
 {
-	int i, j, indices[2];
-	for (i = 0; i < numsSize; i++)
-	{
-		if (target > nums[i])
-		{
-			for (j = 0; j < numsSize; j++)
-			{
-				if (i!=j &&(target - nums[i]) == nums[j] )
-				{
-					indices[0] = i + 1;
-					indices[1] = j + 1;
-					  printf("%d %d",indices[0],indices[1]);
-					return indices;
-				}
+	int i, j;
+ 	int * indices = (int *)malloc(sizeof(int) * 2);
+ 	for (i = 0; i < numsSize; i++)
+ 	{
+ 			for (j = i+1; j < numsSize; j++)
+ 			{
+ 				if (target  == (nums[j]+nums[i]) )
+ 				{
+ 					indices[0] = i ;
+ 					indices[1] = j ;
+					break;
+ 				}
 			}
-		}
-	}
+ 	}
+ 		return indices;
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 int main(int argc, char const *argv[])
 {
 	printf("%s\n", "start");
-	int nums[6] = { 1, 1, 2, 5, 6, 9 };
-	twoSum(nums, 6, 15);
+	int nums[5] = { -1,-2,-3,-4,-5 };
+
+	int *arr=twoSum(nums, 5,-8);
+	printf("%d %d\n",arr[0],arr[1] );
 	return 0;
 
 
