45abc15ef381332afa00f80954a6f2aae2153aa5
talengu <talenapp@gmail.com>
2019-07-31 21:38:22 +0800

    add bubble sort and shell sort shellsort is the best

--- ch02/SortTestHelper.h
+++ ch02/SortTestHelper.h
@@ -27,6 +27,22 @@ namespace SortTestHelper
 		}
 		return arr;
 	}
+	// generate little random array
+	int *generateNearlyOrderedArray(int n,int swapTimes){
+		int *arr=new int[n];
+		for(int i=0;i<n;i++)
+			arr[i]=i;
+		srand(time(NULL));
+		for(int i =0;i<swapTimes;i++){
+			int posx=rand()%n;
+			int posy=rand()%n;
+			swap(arr[posx],arr[posy]);
+
+		}
+		return arr;
+	}
+		
+	 
 	// print function
 	template <typename T>
 		void printArray(T arr[], int n)
@@ -37,6 +53,29 @@ namespace SortTestHelper
 			}
 			cout << endl;
 		}
+	template<typename T>
+		bool isSorted(T arr[],int n){
+			for (int i=0;i<n-1;i++)
+				if (arr[i]>arr[i+1])
+					return false;
+						return true;
+		}
+	template<typename T>
+		void testSort(string sortName,void(*sort)(T[],int),T arr[],int n){
+			// void(*sort)(T[],int) 这个是排序函数， 然后后面这个是测试用例和元素个数
+			clock_t startTime=clock();
+			sort(arr,n);
+				clock_t endTime=clock();
+			assert(isSorted(arr,n));
+			cout<<sortName<<" : " <<double(endTime-startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+			return;
+		}
+// 如果变成模板函数，这里需要一个深拷贝的问题。当输入为一个函数列表的时候。
+	int* copyIntArray(int a[],int n){
+		int* arr=new int[n];
+		copy(a,a+n,arr);
+		return arr;
+	}
 
 } // namespace SortTestHelper
 
--- ch02/main.cpp
+++ ch02/main.cpp
@@ -12,11 +12,10 @@
 
 using namespace std;
 
-using namespace SortTestHelper;// 生成测试数据
 
 /******************************************************************************
-* Function:         
-* Description:      
+* Function:      selectionSort n^2   
+* Description:  选择排序   找到后面的最大值与当前的位置进行交换，排序。 
 * Where:
 * Return:           
 * Error:            
@@ -34,20 +33,118 @@ void selectionSort(T arr[],int n){
 		swap(arr[i],arr[minIndex]);
 	}
 }
+/******************************************************************************
+* Function:         void bubbleSort
+* Description:      
+* Where:
+* Return:           
+* Error:            
+* ref: 优化冒泡法，但是还是比较慢https://blog.csdn.net/ysunflower/article/details/80425788
+*****************************************************************************/
+template<typename T>
+void bubbleSort(T arr[],int n){
+	for (int i=0;i<n-1;i++){
+		for(int j=0;j<n-1;j++){
+			if(arr[j+1]<arr[j]){
+				swap(arr[j+1],arr[j]);
+			}
+		}
+	}
+	
+}
+/******************************************************************************
+* Function:         void insertSort
+* Description:      
+* Where:
+* Return:           
+* Error:            
+*****************************************************************************/
+template<typename T>
+void insertSort(T arr[],int n){
+	for (int i=1;i<n;i++){
+		for (int j=i;j>0&&arr[j]<arr[j-1]; j--) {
+			swap(arr[j],arr[j-1]);
+		}
+	}
+}
+/******************************************************************************
+* Function:         void insertSort2
+* Description:     优化 不使用交换,提前终止,对于有序的数组有优势
+* Where:
+* Return:           
+* Error:            
+*****************************************************************************/
+template<typename T>
+void insertSort2(T arr[],int n){
+	for(int i=0;i<n;i++){
+		T e =arr[i];
+		int j;// j 报错元素 e 应该插入的位置。
+		for(j=i;j>0&&arr[j-1]>e;j--){
+			arr[j]=arr[j-1];
+		}
+		arr[j]=e;
+	}	
+}
+/******************************************************************************
+* Function:         void shellSort
+* Description:      
+* Where:
+* Return:           
+* Error:            
+*****************************************************************************/
+template<typename T>
+void shellSort(T arr[],int n){
+	// 计算 increment sequence:1,4,40,121,364,1093...
+	int h=1;
+	while(h<n/3)
+		h=3*h+1;
 
+	while(h>=1){
+		//h-sort the array
+		for (int i = h;  i < n; i++) {
+			// 对arr[i],arr[i-h],arr[i-2*h,arr[i-2*h]]使用插入排序	
+			T e=arr[i];
+			int j;
+			for(j=i;j>=h&&e<arr[j-h];j-=h)
+				arr[j]=arr[j-h];
+			arr[j]=e;
+		}
+		h/=3;
+}
+	
+}
 int main(int argc, char const *argv[])
 {
-	int n = 100;
-	int *arr = generateRandomArray(n, 0, n);
+	int n = 10000;
+	int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+	int *arr2=SortTestHelper::copyIntArray(arr,n);
+	int *arr3=SortTestHelper::copyIntArray(arr,n);
+	int *arrNearlySorted=SortTestHelper::generateNearlyOrderedArray(n,100);
 
 	float a[5]={1,2,3.2,5,4};
 	Student d[4]={{"A",12},{"c",14},{"C",14},{"D",9}};
+// 测试排序时间	
+	SortTestHelper::testSort("Selection Sort",selectionSort,arr,n);
+
+	SortTestHelper::testSort("Insert Sort",insertSort,arr2,n);
+
+	SortTestHelper::testSort("Insert Sort2",insertSort2,arr3,n);
+// 对于近乎有序的数据，插入排序，比后面的nlogn的算法会更好。比如服务器日志等的对时间排序。 它会变成O n 级别的算法。	
+	SortTestHelper::testSort("Insert Sort2",insertSort2,arrNearlySorted,n);
+
+	int *arrBubble=SortTestHelper::generateRandomArray(n,0,n);
+	SortTestHelper::testSort("bubble Sort",bubbleSort,arrBubble,n);
+	
+	int *arrShell=SortTestHelper::generateRandomArray(n,0,n);
+	SortTestHelper::testSort("shell Sort",shellSort,arrShell,n);
+	
 
 
-	selectionSort(d,4);
-
-	printArray(d, 4);
+//	SortTestHelper::printArray(d, 4);
 
 	delete[] arr;
+	delete[] arr2;
+	delete[] arr3;
+	delete[] arrNearlySorted;
 	return 0;
 }
