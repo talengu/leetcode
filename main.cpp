63548fdb34002dd028d618630810a98a63eaf4f2
talengu <talenapp@gmail.com>
2019-08-14 23:21:51 +0800

    update main

--- main.cpp
+++ main.cpp
@@ -11,11 +11,13 @@ int main()
     freopen("in.txt", "r", stdin);
 // freopen("C:\\Users\\TaoSama\\Desktop\\out.txt","w",stdout);
 #endif
-    int a=1;
-    int c=2*a;
-    string aa;
-    cin >> a;
-    cin >> aa;
-    cout << aa + " " << a << endl;
-    return 0;
+
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x; scanf("%d",&x);
+		i==(n-1)?cout<<n+1-x<<endl:cout<<n+1-x<<" ";
+    }
+
+
 }
--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import numpy as np
+import sys
 
-A=input()
-print(A)
+# def three(three_list):
+#     // 
+
+# n = int(sys.stdin.readline().strip())
+
+# nums = [int(item) for item in sys.stdin.readline().split()]
+
+# min_idx=nums.index(min(nums))
+
+# lis=list(enumerate(nums))
+
+
+# res=100
+
+
+# for item in nums[min_idx+1:]:
+
+N, K = [int(item) for item in sys.stdin.readline().split()]
+
+
+
+
+
+        
+
+
+    
+    
+    
