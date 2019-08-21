b2f39830f29920f071a82bcbe9270032fe920cc8
talengu <talenapp@gmail.com>
2019-08-22 01:22:57 +0800

    fix: python use in.txt as input

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 # -*- coding: utf-8 -*-
 import sys
 
-# def three(three_list):
-#     // 
+sys.stdin = open('in.txt')
 
-# n = int(sys.stdin.readline().strip())
+# N= [item for item in sys.stdin.readline().strip().split()]
+# low,high= [int(item) for item in sys.stdin.readline().strip().split()]
+# t=int(sys.stdin.readline().strip())
 
-# nums = [int(item) for item in sys.stdin.readline().split()]
+print(sys.stdin.readline())
+print(sys.stdin.readline())
 
-# min_idx=nums.index(min(nums))
-
-# lis=list(enumerate(nums))
-
-
-# res=100
-
-
-# for item in nums[min_idx+1:]:
-
-N, K = [int(item) for item in sys.stdin.readline().split()]
-
-
-
-
-
-        
-
-
-    
-    
-    
