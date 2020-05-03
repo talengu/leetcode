10cefef067e07466ba7aa2b6f9bc043f96ba156e
talengu <talenapp@gmail.com>
2020-05-03 09:30:38 +0800

    moban 编辑

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+
+sys.stdin = open('in.txt')
+
+# N= [item for item in sys.stdin.readline().strip().split()]
+# low,high= [int(item) for item in sys.stdin.readline().strip().split()]
+# t=int(sys.stdin.readline().strip())
+
+print(sys.stdin.readline())
+print(sys.stdin.readline())
+
