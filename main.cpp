0677656590c6eba1e1685bff385f5514e366a2fe
talengu <talenapp@gmail.com>
2020-05-03 10:01:04 +0800

    use vim edit

--- main.cpp
+++ main.cpp
@@ -1,21 +1,35 @@
+// 13. 罗马数字转整数
+// https://leetcode-cn.com/problems/roman-to-integer/
 #include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-//#define LOCAL
+ class Solution {
+ public:
+     int romanToInt(string s) {
+         unordered_map<string, int> m = {{"I", 1}, {"IV", 3}, {"IX", 8}, {"V", 5}, {"X", 10}, {"XL", 30}, {"XC", 80}, {"L", 50}, {"C", 100}, {"CD", 300}, {"CM", 800}, {"D", 500}, {"M", 1000}};
+         int r = m[s.substr(0, 1)];
+         for(int i=1; i<s.size(); ++i){
+             string two = s.substr(i-1, 2);
+             string one = s.substr(i, 1);
+             r += m[two] ? m[two] : m[one];
+        }
+         return r;
+     }
+ };
+
+
+#define LOCAL
 int main()
 {
 #ifndef LOCAL
     freopen("in.txt", "r", stdin);
 // freopen("C:\\Users\\TaoSama\\Desktop\\out.txt","w",stdout);
 #endif
-    int a=1;
-    int c=2*a;
-    string aa;
-    cin >> a;
-    cin >> aa;
-    cout << aa + " " << a << endl;
+     Solution mSolution;
+     cout << mSolution.romanToInt("IX") << endl;
     return 0;
 }
